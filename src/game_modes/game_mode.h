50a784ebd2350efd12e8c4b1b83465469058ea7d
Luis Ayala <luisayala@Luiss-MacBook-Pro.local>
2022-03-20 18:39:22 -0700

    included entities in game_mode

--- src/game_modes/game_mode.h
+++ src/game_modes/game_mode.h
@@ -1,8 +1,22 @@
 #ifndef GAME_MODE_H
 #define GAME_MODE_H
 
+#include <string>
+
+#include "player.h"
+
 namespace tournament {
-class GameMode {}
+class GameMode {
+ public:
+  virtual bool IsDone() = 0;
+  virtual void PlayRound() = 0;
+  virtual void NextRound() = 0;
+  virtual Player GetChampion() = 0;  // TODO must return Player entity
+
+ private:
+  std::string mode_name_;
+  int round_num_;
+}
 }  // namespace tournament
 
 #endif
